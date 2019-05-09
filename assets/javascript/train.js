539ccb67942a197bb435c951c4c0980f009d9471
MattJ <47984960+mjalbert13@users.noreply.github.com>
2019-05-09 18:29:11 -0400

    started moment js

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -22,10 +22,10 @@ var firebaseConfig = {
 
       var trainName = $("#train-name-input").val();
       var trainDestination = $("#destination-input").val();
-      var trainStart = $("#train-start-input").val();
+      var trainStart = moment($("#train-start-input").val().trim(),"HH:mm").format("X");
       var trainFrequency = $("#frequency-input").val();
 
-      console.log(trainName,trainDestination,trainStart,trainFrequency);
+    //   console.log(trainName,trainDestination,trainStart,trainFrequency);
     
       var newTrain = {
           train: trainName,
@@ -45,21 +45,35 @@ var firebaseConfig = {
 
 database.ref().on('child_added', function(childSanpshot){
 
-    console.log(childSanpshot.val());
 
     var newTrain = childSanpshot.val().train;
     var trainDestination = childSanpshot.val().destination;
     var trainStart = childSanpshot.val().firstTrain;
     var trainFrequency = childSanpshot.val().frequency;
 
-    console.log(newTrain, trainDestination,trainStart, trainFrequency);
+    var time = moment().valueOf();
+    console.log(moment(time).format("HH:mm"));
+    console.log(trainStart);
+    
+    var formatTime = moment(trainStart, "hh:mm").subtract(1,"years");
+    console.log(formatTime);
+    
+    var trainTime = moment().diff(moment(trainStart, "X"),"minutes");
+    console.log(trainTime);
+    
+    var niceTrain = moment(trainTime, "hh:mm").subtract(1, "years");
+    console.log(niceTrain);
 
+    var nextTrain;
+    
+    var arivesIn = nextTrain - time;
+    
     var row = $("<tr>").append(
         $("<td>").text(newTrain),
         $("<td>").text(trainDestination),
         $("<td>").text(trainFrequency),
-        $("<td>").text("Next Train"),
-        $("<td>").text("Arives in...")
+        $("<td>").text(nextTrain),
+        $("<td>").text(arivesIn)
     )
     
     $("#train-table > tbody").append(row);
